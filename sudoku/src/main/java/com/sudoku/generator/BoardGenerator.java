904bb2273e54e1937dad34bb1aa3a4b71207546e
hoangcongtbinh <hoangcongtbinh@gmail.com>
2026-05-29 21:53:30 +0700

    Update BoardGenerator output

--- sudoku/src/main/java/com/sudoku/generator/BoardGenerator.java
+++ sudoku/src/main/java/com/sudoku/generator/BoardGenerator.java
@@ -3,7 +3,7 @@ package com.sudoku.generator;
 import com.sudoku.model.*;
 
 public class BoardGenerator {
-    Board generateBoard(Difficulty difficulty) {
+    GameSession generateBoard(Difficulty difficulty) {
         /*
         Thực hiện tạo ra 1 bảng Sudoku mới tùy theo độ khó
         Phải đảm bảo chỉ có 1 cách giải duy nhất cho bảng
