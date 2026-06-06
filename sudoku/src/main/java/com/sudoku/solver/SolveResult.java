fdf58819ac8600be43b9482fe7d42ae4c5c123c9
24022333 <24022333@vnu.edu.vn>
2026-06-07 00:18:26 +0700

    update

--- sudoku/src/main/java/com/sudoku/solver/SolveResult.java
+++ sudoku/src/main/java/com/sudoku/solver/SolveResult.java
@@ -1,13 +1,41 @@
 package com.sudoku.solver;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import com.sudoku.model.*;
+import com.sudoku.model.Step;
 
 public class SolveResult {
-    boolean solved;
-    List<Step> steps;
-    long timeToSolve;
 
-    // Để các thuộc tính Private, viết thêm hàm get/set và các hàm khác nếu cần thiết
+    private boolean solved;
+    private List<Step> steps = new ArrayList<>();
+    private long timeToSolve;
+
+    public boolean isSolved() {
+        return solved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
+    }
+
+    public List<Step> getSteps() {
+        return Collections.unmodifiableList(steps);
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public void addStep(Step step) {
+        this.steps.add(step);
+    }
+
+    public long getTimeToSolve() {
+        return timeToSolve;
+    }
+
+    public void setTimeToSolve(long timeToSolve) {
+        this.timeToSolve = timeToSolve;
+    }
 }
-
